8686b728190d205d48df115bde965af923abac4a
Jacob Poul Richardt <jacob@geckon.com>
2012-04-24 15:59:18 +0200

    Added ObjectRelation.

---
+++ src/app/CHAOS.Portal.Client (.NET)/Extensions/IObjectRelationExtension.cs
@@ -0,0 +1,11 @@
+﻿using CHAOS.Portal.Client.Data;
+using CHAOS.Portal.Client.ServiceCall;
+
+namespace CHAOS.Portal.Client.Extensions
+{
+	public interface IObjectRelationExtension
+	{
+		IServiceCallState<IServiceResult_MCM<ScalarResult>> Create(UUID object1GUID, UUID object2GUID, uint objectRelationTypeID, int? sequence);
+		IServiceCallState<IServiceResult_MCM<ScalarResult>> Delete(UUID object1GUID, UUID object2GUID, uint objectRelationTypeID);
+	}
+}
---
+++ src/app/CHAOS.Portal.Client.Standard (.NET)/Extension/ObjectRelationExtension.cs
@@ -0,0 +1,23 @@
+﻿using CHAOS.Portal.Client.Data;
+using CHAOS.Portal.Client.Extensions;
+using CHAOS.Portal.Client.ServiceCall;
+using CHAOS.Portal.Client.Standard.ServiceCall;
+using CHAOS.Web;
+
+namespace CHAOS.Portal.Client.Standard.Extension
+{
+	public class ObjectRelationExtension : AExtension, IObjectRelationExtension
+	{
+		public ObjectRelationExtension(IServiceCaller serviceCaller) : base(serviceCaller) {}
+
+		public IServiceCallState<IServiceResult_MCM<ScalarResult>> Create(UUID object1GUID, UUID object2GUID, uint objectRelationTypeID, int? sequence)
+		{
+			return CallService<IServiceResult_MCM<ScalarResult>>(HTTPMethod.GET, object1GUID, object2GUID, objectRelationTypeID, sequence);
+		}
+
+		public IServiceCallState<IServiceResult_MCM<ScalarResult>> Delete(UUID object1GUID, UUID object2GUID, uint objectRelationTypeID)
+		{
+			return CallService<IServiceResult_MCM<ScalarResult>>(HTTPMethod.GET, object1GUID, object2GUID, objectRelationTypeID);
+		}
+	}
+}
