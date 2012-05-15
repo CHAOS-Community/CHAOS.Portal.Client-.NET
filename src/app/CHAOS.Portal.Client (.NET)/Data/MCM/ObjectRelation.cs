84f8dc8e997f0590227e1c50ab8c3e0d00a7d73a
Jacob Poul Richardt <jacob@geckon.com>
2012-05-15 23:21:14 +0200

    Updated ObjectRelation.

--- src/app/CHAOS.Portal.Client (.NET)/Data/MCM/ObjectRelation.cs
+++ src/app/CHAOS.Portal.Client (.NET)/Data/MCM/ObjectRelation.cs
@@ -1,7 +1,17 @@
+using System;
+
 namespace CHAOS.Portal.Client.Data.MCM
 {
 	public class ObjectRelation
 	{
-		
+		public Guid Object1GUID { get; set; }
+
+		public Guid Object2GUID { get; set; }
+
+		public uint ObjectRelationTypeID { get; set; }
+
+		public int? Sequence { get; set; }
+
+		public DateTime DateCreated { get; set; }
 	}
 }
