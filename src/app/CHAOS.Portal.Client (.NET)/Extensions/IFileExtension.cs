85972d8eb2d2548b1c14e696dca8da387ea08cac
Peter Overgaard <peter@geckon.com>
2012-06-27 14:59:25 +0200

    Added File/Create

---
+++ src/app/CHAOS.Portal.Client (.NET)/Extensions/IFileExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Xml.Linq;
+using CHAOS.Portal.Client.Data;
+using CHAOS.Portal.Client.Data.MCM;
+using CHAOS.Portal.Client.ServiceCall;
+
+namespace CHAOS.Portal.Client.Extensions
+{
+    public interface IFileExtension
+    {
+        IServiceCallState<IServiceResult_MCM<File>> Create(Guid objectGUID, uint? parentFileID, uint formatID, uint destinationID, string filename, string originalFilename, string folderPath);
+    }
+}
