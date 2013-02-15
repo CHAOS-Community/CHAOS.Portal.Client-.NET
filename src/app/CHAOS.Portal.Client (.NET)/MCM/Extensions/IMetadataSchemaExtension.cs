4029a20810715815687c45bc894b97a6ca21ccbe
Jacob Poul Richardt <jacob@geckon.com>
2013-02-15 13:47:52 +0100

    Added new MetadataSchema methods.

--- src/app/CHAOS.Portal.Client (.NET)/MCM/Extensions/IMetadataSchemaExtension.cs
+++ src/app/CHAOS.Portal.Client (.NET)/MCM/Extensions/IMetadataSchemaExtension.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Xml.Linq;
+using CHAOS.Portal.Client.Data;
 using CHAOS.Portal.Client.MCM.Data;
 using CHAOS.Portal.Client.ServiceCall;
 
@@ -7,5 +9,8 @@ namespace CHAOS.Portal.Client.MCM.Extensions
 	public interface IMetadataSchemaExtension
 	{
 		IServiceCallState<MetadataSchema> Get(Guid? guid = null);
+		IServiceCallState<MetadataSchema> Create(string name, XElement schemaXml, Guid? guid = null);
+		IServiceCallState<MetadataSchema> Update(string name, XElement schemaXml, Guid guid);
+		IServiceCallState<ScalarResult> Delete(Guid guid);
 	}
 }
--- src/app/CHAOS.Portal.Client (.NET)/MCM/Extensions/MetadataSchemaExtension.cs
+++ src/app/CHAOS.Portal.Client (.NET)/MCM/Extensions/MetadataSchemaExtension.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Xml.Linq;
+using CHAOS.Portal.Client.Data;
 using CHAOS.Portal.Client.Extensions;
 using CHAOS.Portal.Client.MCM.Data;
 using CHAOS.Portal.Client.ServiceCall;
@@ -10,6 +12,21 @@ namespace CHAOS.Portal.Client.MCM.Extensions
 		public IServiceCallState<MetadataSchema> Get(Guid? guid)
 		{
 			return CallService<MetadataSchema>(HTTPMethod.GET, guid);
+		}
+
+		public IServiceCallState<MetadataSchema> Create(string name, XElement schemaXml, Guid? guid = null)
+		{
+			return CallService<MetadataSchema>(HTTPMethod.POST, name, schemaXml, guid);
+		}
+
+		public IServiceCallState<MetadataSchema> Update(string name, XElement schemaXml, Guid guid)
+		{
+			return CallService<MetadataSchema>(HTTPMethod.POST, name, schemaXml, guid);
+		}
+
+		public IServiceCallState<ScalarResult> Delete(Guid guid)
+		{
+			return CallService<ScalarResult>(HTTPMethod.GET, guid);
 		}
 	}
 }
